e9df265143bfba3ba6d11bf898b64599cda2183d
RaccPark <p48754250@gmail.com>
2024-09-20 02:41:27 +0900

    0920 불필요 텍스처 삭제 및 Discharge UI 추가

--- Source/PeeeP_Prototype/UI/PPKeyManualUI.cpp
+++ Source/PeeeP_Prototype/UI/PPKeyManualUI.cpp
@@ -12,11 +12,13 @@ void UPPKeyManualUI::NativeConstruct()
 	KeyManualImage_WASD = Cast<UImage>(GetWidgetFromName(TEXT("KeyManualImage_WASD")));					// 키 가이드 이미지 - WASD
 	KeyManualImage_Jump = Cast<UImage>(GetWidgetFromName(TEXT("KeyManualImage_Jump")));					// 키 가이드 이미지 - Jump
 	KeyManualImage_Charge = Cast<UImage>(GetWidgetFromName(TEXT("KeyManualImage_Charge")));				// 키 가이드 이미지 - Charge
+	KeyManualImage_Discharge = Cast<UImage>(GetWidgetFromName(TEXT("KeyManualImage_Discharge")));		// 키 가이드 이미지 - Discharge
 
 
 	KeyManualImage_WASD->SetVisibility(ESlateVisibility::Hidden);
 	KeyManualImage_Jump->SetVisibility(ESlateVisibility::Hidden);
 	KeyManualImage_Charge->SetVisibility(ESlateVisibility::Hidden);
+	KeyManualImage_Discharge->SetVisibility(ESlateVisibility::Hidden);
 
 	// 델리게이트를 위한 인터페이스(플레이어에게 상속됨)
 	IPPElectricHUDInterface* ElectricHUDInterface = Cast<IPPElectricHUDInterface>(GetOwningPlayerPawn());
@@ -32,8 +34,15 @@ void UPPKeyManualUI::SetAllKeyManualImageHidden()
 	KeyManualImage_WASD->SetVisibility(ESlateVisibility::Hidden);
 	KeyManualImage_Jump->SetVisibility(ESlateVisibility::Hidden);
 	KeyManualImage_Charge->SetVisibility(ESlateVisibility::Hidden);
+	KeyManualImage_Discharge->SetVisibility(ESlateVisibility::Hidden);
 }
 
+/// <summary>
+/// 키 가이드 UI 표시, 입력한 index에 해당하는 하나의 UI만 표시
+/// ///
+/// 0: 모두 비활성화, 1: 기본 조작, 2: 점프, 3: 전기 충전, 4: 전기 방출
+/// </summary>
+/// <param name="index">표시할 UI</param>
 void UPPKeyManualUI::SetKeyManualImageVisible(uint32 index)
 {
 	SetAllKeyManualImageHidden();
@@ -56,6 +65,10 @@ void UPPKeyManualUI::SetKeyManualImageVisible(uint32 index)
 		KeyManualImage_Charge->SetVisibility(ESlateVisibility::Visible);
 	}
 		break;
+	case 4:
+	{
+		KeyManualImage_Discharge->SetVisibility(ESlateVisibility::Visible);
+	}
 	default:
 		break;
 	}

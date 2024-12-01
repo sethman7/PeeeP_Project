6eb6399203310449acc882bec14e5bdbc4e1427b
RaccPark <p48754250@gmail.com>
2024-12-01 19:42:50 +0900

    1201 1차 인벤토리 작업(미완)

--- Source/PeeeP_Prototype/Inventory/PPInventoryComponent.cpp
+++ Source/PeeeP_Prototype/Inventory/PPInventoryComponent.cpp
@@ -2,15 +2,26 @@
 
 
 #include "Inventory/PPInventoryComponent.h"
+#include "Engine/AssetManager.h"
 
 // Sets default values for this component's properties
 UPPInventoryComponent::UPPInventoryComponent()
 {
-	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
-	// off to improve performance if you don't need them.
-	PrimaryComponentTick.bCanEverTick = true;
+	bWantsInitializeComponent = true;
 
-	// ...
+	MaxInventorySlotNum = 6;
+	MaxItemNum = 1;
+
+	// 초기 사이즈 지정
+	PartsItems.Init(nullptr, MaxInventorySlotNum);
+}
+
+void UPPInventoryComponent::InitializeComponent()
+{
+	Super::InitializeComponent();
+
+	// 인벤토리 초기화
+	InitInventory();
 }
 
 
@@ -23,6 +34,114 @@ void UPPInventoryComponent::BeginPlay()
 	
 }
 
+
+bool UPPInventoryComponent::AddItem(FName InItemName, int32 InItemQuantity, int32& OutItemQuantity)
+{
+	// 성공적으로 추가했는지에 대한 결과 반환용 변수
+	bool bIsResult = false;
+
+	// 애셋 매니저 불러오기
+	UAssetManager& Manager = UAssetManager::Get();
+
+	// 애셋 아이디 리스트 받아오기
+	TArray<FPrimaryAssetId> Assets;
+	// 태그 정보를 넘겨줘서 동일한 태그를 가진 애셋들의 목록을 배열로 변환받음
+	Manager.GetPrimaryAssetIdList(TEXT("PPPartsData"), Assets);
+
+	// 특정 아이템 키 생성
+	FPrimaryAssetId Key;
+	Key.PrimaryAssetType = TEXT("PPPartsData");
+	Key.PrimaryAssetName = InItemName;
+
+	// 해당 키의 애셋이 존재한다면
+	if (Assets.Contains(Key))
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Find Data"));
+
+		// 아이템 생성
+		UPPInventoryPartsItem* NewItem = NewObject<UPPInventoryPartsItem>();
+		if (NewItem)
+		{
+			FSoftObjectPtr AssetPtr(Manager.GetPrimaryAssetPath(Assets.FindByKey(Key)[0]));
+			if (AssetPtr.IsPending())
+			{
+				AssetPtr.LoadSynchronous();
+			}
+			UPPPartsDataBase* ItemData = Cast<UPPPartsDataBase>(AssetPtr.Get());
+			if (ItemData)
+			{
+				NewItem->PartsData = ItemData;
+				NewItem->ItemQuantity = InItemQuantity;
+			}
+			else
+			{
+				return bIsResult;
+			}
+		}
+	}
+
+}
+
+void UPPInventoryComponent::UseItem(int32 InSlotIndex)
+{
+}
+
+void UPPInventoryComponent::InitInventory()
+{
+	// 기존에 있던 정보들은 어디서 가져올 것인가?
+
+	// 에셋 매니저 싱글톤 불러오기
+	UAssetManager& Manager = UAssetManager::Get();
+
+	// 에셋 아이디 리스트 받아오기
+	TArray<FPrimaryAssetId> Assets;
+	// 태그 정보를 넘겨줘서 동일한 태그를 가진 애셋들의 목록을 배열로 반환받음
+	Manager.GetPrimaryAssetIdList(TEXT("PPPartsData"), Assets);
+
+	if (Assets.Num() > 0)
+	{
+		TMap<int32, TPair<FName, int32>> InventoryPartsArray;
+		// 테스트 블록(실제로는 저장된 파일에서 데이터를 읽어와야 함)
+		{
+			InventoryPartsArray.Add(1, { TEXT("GrabParts"), 1 });
+		}
+
+		for (const auto& InvItem : InventoryPartsArray)
+		{
+			// 특정 아이템 키 생성
+			FPrimaryAssetId Key;
+			Key.PrimaryAssetType = TEXT("PPPartsData");
+			Key.PrimaryAssetName = InvItem.Value.Key;
+
+			if (Assets.Contains(Key))
+			{
+				// 아이템 생성
+				UPPInventoryPartsItem* NewItem = NewObject<UPPInventoryPartsItem>();
+				if (NewItem)
+				{
+					FSoftObjectPtr AssetPtr(Manager.GetPrimaryAssetPath(Assets.FindByKey(Key)[0]);
+					if (AssetPtr.IsPending())
+					{
+						AssetPtr.LoadSynchronous();
+					}
+					UPPPartsDataBase* ItemData = Cast<UPPPartsDataBase>(AssetPtr.Get());
+					if (ItemData)
+					{
+						NewItem->PartsData = ItemData;
+						NewItem->ItemQuantity = InvItem.Value.Value;
+						// 아이템 넣기
+						PartsItems[InvItem.Key] = NewItem;
+					}
+				}
+			}
+		}
+
+	}
+}
+
+void UPPInventoryComponent::RemoveItem(int32 InSlotIndex)
+{
+}
 
 // Called every frame
 void UPPInventoryComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
--- Source/PeeeP_Prototype/Inventory/PPInventoryComponent.h
+++ Source/PeeeP_Prototype/Inventory/PPInventoryComponent.h
@@ -4,8 +4,11 @@
 
 #include "CoreMinimal.h"
 #include "Components/ActorComponent.h"
+#include "PPInventoryPartsItem.h"
 #include "PPInventoryComponent.generated.h"
 
+// 인벤토리 내용 변경 델리게이트
+DECLARE_MULTICAST_DELEGATE(FonChangedInventoryDelegate);
 
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
 class PEEEP_PROTOTYPE_API UPPInventoryComponent : public UActorComponent
@@ -16,9 +19,38 @@ public:
 	// Sets default values for this component's properties
 	UPPInventoryComponent();
 
+	virtual void InitializeComponent() override;
+
 protected:
 	// Called when the game starts
 	virtual void BeginPlay() override;
+
+	// 파츠
+	UPROPERTY(VisibleAnywhere, Category = "Inventory", meta = (AllowPrivateAccess = "true"))
+	TArray<TObjectPtr<class UPPInventoryPartsItem>> PartsItems;
+
+	// 최대 인벤토리 슬롯 수
+	UPROPERTY(EditAnywhere, Category = "Inventory", meta = (AllowPrivateAccess = "true"))
+	int32 MaxInventorySlotNum;
+
+	// 슬롯 한 칸당 저장 가능한 아이템의 수(파츠는 1 고정)
+	UPROPERTY(VisibleAnywhere, Category = "Inventory", meta = (AllowPrivateAccess = "true"))
+	int32 MaxItemNum;
+
+public:
+	// Getter
+	FORCEINLINE TArray<TObjectPtr<class UPPInventoryPartsItem>> GetPartsItems() { return PartsItems; }
+
+	// 아이템(파츠) 추가
+	bool AddItem(FName InItemName, int32 InItemQuantity, int32& OutItemQuantity);
+	// 아이템(파츠) 사용
+	void UseItem(int32 InSlotIndex);
+
+protected:
+	// 인벤토리 초기화
+	void InitInventory();
+	// 아이템 삭제
+	void RemoveItem(int32 InSlotIndex);
 
 public:	
 	// Called every frame
